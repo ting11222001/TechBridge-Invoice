23dc996814455b3bf59bbb210161a9930bd32910
Li-Ting Liao <39208708+ting11222001@users.noreply.github.com>
2026-02-16 15:32:13 +1030

    Fix the bcrypt encoder

---
+++ 03-application-yml.md
@@ -0,0 +1,3 @@
+sql:
+ init:
+   mode: never → change to "always" when you want to run schema.sql again.
--- src/main/java/io/techbridge/invoice/techbridge_invoice/TechbridgeInvoiceApplication.java
+++ src/main/java/io/techbridge/invoice/techbridge_invoice/TechbridgeInvoiceApplication.java
@@ -2,12 +2,19 @@ package io.techbridge.invoice.techbridge_invoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TechbridgeInvoiceApplication {
+	private static final int STRENGTH = 12;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechbridgeInvoiceApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(STRENGTH);
+	}
 }
